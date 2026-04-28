911d780a5946902196da26b64135efbd813d0a2c
parv-nangalia <nangaliaparv@gmail.com>
2026-04-28 19:32:54 +0530

    fix: BASE PATH changes
    - changed vite config and .yml base path

--- portfolio/artifacts/portfolio-3d/vite.config.ts
+++ portfolio/artifacts/portfolio-3d/vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
-  base: process.env.BASE_PATH || "/",
+  base: process.env.BASE_PATH || "parv-nangalia/portfolio/artifacts/portfolio-3d/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
